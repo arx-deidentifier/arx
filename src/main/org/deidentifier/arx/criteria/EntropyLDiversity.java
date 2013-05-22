454b5bc6ff6f306c80dd7908a6fb897dc32305e5
Kohlmayer <kohlmayer@gmail.com>
2013-05-22 09:57:41 +0200

    simplified entropy-ldiversity check

--- src/main/org/deidentifier/arx/criteria/EntropyLDiversity.java
+++ src/main/org/deidentifier/arx/criteria/EntropyLDiversity.java
@@ -25,20 +25,20 @@ import org.deidentifier.arx.framework.check.groupify.HashGroupifyEntry;
  * The entropy l-diversity criterion
  * @author Prasser, Kohlmayer
  */
-public class EntropyLDiversity extends LDiversity{
+public class EntropyLDiversity extends LDiversity {
 
-    private static final long serialVersionUID = -354688551915634000L;
+    private static final long   serialVersionUID = -354688551915634000L;
 
     /** Helper*/
-    private final double logL;
+    private final double        logL;
     /** Helper*/
-    private static final double  log2 = Math.log(2);
-    
+    private static final double log2             = Math.log(2);
+
     /**
      * Creates a new instance
      * @param l
      */
-    public EntropyLDiversity(int l){
+    public EntropyLDiversity(int l) {
         super(l);
         logL = Math.log(l) / Math.log(2d);
     }
@@ -47,35 +47,40 @@ public class EntropyLDiversity extends LDiversity{
     public boolean isAnonymous(HashGroupifyEntry entry) {
 
         Distribution d = entry.distribution;
-        
+
         // If less than l values are present skip
         if (d.size() < l) { return false; }
 
-        // Copy and pack
-        int totalElements = 0;
-        // TODO: Why do we need a copy here???
-        final int[] frequencyCopy = new int[d.size()];
-        int count = 0;
-        int[] buckets = d.getBuckets();
+        // Count number of elements in entrys distribution
+        double totalElements = 0;
+        double sum1 = 0d;
+
+        final int[] buckets = d.getBuckets();
         for (int i = 0; i < buckets.length; i += 2) {
             if (buckets[i] != -1) { // bucket not empty
-                final int frequency = buckets[i + 1];
-                frequencyCopy[count++] = frequency;
+                final double frequency = buckets[i + 1];
+                sum1 += frequency * log2(frequency);
                 totalElements += frequency;
             }
         }
 
-        double val = 0d;
-        for (int i = 0; i < frequencyCopy.length; i++) {
-            final double p = ((double) frequencyCopy[i] / (double) totalElements);
-            val += p * log2(p);
-        }
+        double val = (1d / totalElements) * sum1 - log2(totalElements) * totalElements;
+
+        // // compute entropy
+        // double val = 0d;
+        // for (int i = 0; i < buckets.length; i += 2) {
+        // if (buckets[i] != -1) { // bucket not empty
+        // final int frequency = buckets[i + 1];
+        // final double p = ((double) frequency / (double) totalElements);
+        // val += p * log2(p);
+        // }
+        // }
         val = -val;
 
         // check
         return val >= logL;
-    } 
-    
+    }
+
     /**
      * Computes log 2
      * 
