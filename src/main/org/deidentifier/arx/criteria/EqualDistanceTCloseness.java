9503aeb3fecab9c8765b1eb7717549f5b13ffc28
Kohlmayer <kohlmayer@gmail.com>
2013-05-22 11:45:07 +0200

    optimized equaldistance l-diverity (omit copying)

--- src/main/org/deidentifier/arx/criteria/EqualDistanceTCloseness.java
+++ src/main/org/deidentifier/arx/criteria/EqualDistanceTCloseness.java
@@ -30,7 +30,7 @@ public class EqualDistanceTCloseness extends TCloseness {
     private static final long serialVersionUID = -1383357036299011323L;
 
     /** The original distribution*/
-    private double[]        distribution;
+    private double[]          distribution;
 
     /**
      * Creates a new instance
@@ -49,25 +49,21 @@ public class EqualDistanceTCloseness extends TCloseness {
     public boolean isAnonymous(HashGroupifyEntry entry) {
 
         // calculate emd with equal distance
-        final int[] calcArray = new int[distribution.length];
 
         int[] buckets = entry.distribution.getBuckets();
         int totalElements = 0;
         for (int i = 0; i < buckets.length; i += 2) {
             if (buckets[i] != -1) { // bucket not empty
-                final int value = buckets[i];
-                final int frequency = buckets[i + 1];
-                calcArray[value] = frequency;
-                totalElements += frequency;
+                totalElements += buckets[i + 1];
             }
         }
 
         double val = 0d;
-
-        for (int i = 0; i < calcArray.length; i++) {
-            val += Math.abs((distribution[i] - ((double) calcArray[i] / (double) totalElements)));
+        for (int i = 0; i < buckets.length; i += 2) {
+            if (buckets[i] != -1) { // bucket not empty
+                val += Math.abs((distribution[buckets[i]] - ((double) buckets[i + 1] / (double) totalElements)));
+            }
         }
-
         val /= 2;
 
         // check
