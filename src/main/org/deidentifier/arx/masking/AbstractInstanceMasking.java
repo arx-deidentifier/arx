5ca83156238e6bcd4d389ef64e57dac7a0343f89
prasser <mail@fabian-prasser.de>
2013-07-24 11:43:53 +0200

    Remove masking from ARX master

--- src/main/org/deidentifier/arx/masking/AbstractInstanceMasking.java
+++
@@ -1,8 +0,0 @@
-package org.deidentifier.arx.masking;
-
-import org.deidentifier.arx.DataType;
-
-public abstract class AbstractInstanceMasking<T extends DataType<?>> extends AbstractMaskingOperator<T> {
-
-	public abstract String mask(String input);
-}
