161f49c98d81e17c1255c93b4461e71abd2e59c1
Karol Babioch <karol@babioch.de>
2014-03-17 00:00:38 +0100

    gui: wizardimport: Implement first draft of XLS page

---
+++ src/gui/org/deidentifier/arx/gui/view/impl/importwizard/WizardImportXlsPage.java
@@ -0,0 +1,93 @@
+package org.deidentifier.arx.gui.view.impl.importwizard;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+
+public class WizardImportXlsPage extends WizardPage {
+
+    private WizardImportData data;
+
+    private Label lblLocation;
+    private Combo comboLocation;
+    private Button btnChoose;
+
+
+    public WizardImportXlsPage(WizardImportData data)
+    {
+
+        super("WizardImportXlsPage");
+
+        setTitle("XLS");
+        setDescription("Please provide the information requested below");
+
+        this.data = data;
+
+    }
+
+    public void createControl(Composite parent)
+    {
+
+        Composite container = new Composite(parent, SWT.NULL);
+
+        setControl(container);
+        container.setLayout(new GridLayout(3, false));
+
+        lblLocation = new Label(container, SWT.NONE);
+        lblLocation.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+        lblLocation.setText("Location");
+
+        comboLocation = new Combo(container, SWT.READ_ONLY);
+        comboLocation.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+        comboLocation.addSelectionListener(new SelectionAdapter() {
+
+            @Override
+            public void widgetSelected(SelectionEvent arg0) {
+
+            }
+
+        });
+
+        btnChoose = new Button(container, SWT.NONE);
+        btnChoose.setText("Browse...");
+        btnChoose.addSelectionListener(new SelectionAdapter() {
+
+            @Override
+            public void widgetSelected(SelectionEvent arg0) {
+
+                setPageComplete(false);
+                setErrorMessage(null);
+
+                final String path = data.getWizard().getController().actionShowOpenFileDialog("*.xls");
+
+                if (path == null) {
+
+                    return;
+
+                }
+
+                if (comboLocation.indexOf(path) == -1) {
+
+                    comboLocation.add(path, 0);
+
+                }
+
+                comboLocation.select(comboLocation.indexOf(path));
+
+            }
+
+        });
+
+        setPageComplete(false);
+
+    }
+
+}
