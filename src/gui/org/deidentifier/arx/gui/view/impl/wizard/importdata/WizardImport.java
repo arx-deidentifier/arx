29c4b3a7cb34987c4a01dd0823b112ff6841170d
Karol Babioch <karol@babioch.de>
2014-03-17 19:38:17 +0100

    gui: wizard: importdata: Add javadoc comments to WizardImport.java

--- src/gui/org/deidentifier/arx/gui/view/impl/wizard/importdata/WizardImport.java
+++ src/gui/org/deidentifier/arx/gui/view/impl/wizard/importdata/WizardImport.java
@@ -7,13 +7,43 @@ import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 
 
+/**
+ * Wizard guiding the user through the process of importing data into the GUI
+ *
+ * The user is taken through the process of importing data into the GUI step by
+ * step. This includes the selection of the data source as well as the inquiry
+ * for all of the information coming along with it.
+ *
+ * Multiple sources are supported for the import are supported:
+ *
+ * <ul>
+ *  <li>{@link WizardImportCsvPage} CSV</li>
+ *  <li>{@link WizardImportJdbcPage} Database (JDBC)</li>
+ *  <li>{@link WizardImportXlsPage} Excel</li>
+ * </ul>
+ *
+ * Refer to appropriate page(s) for more details about a specific source.
+ */
 public class WizardImport extends Wizard {
 
+    /**
+     * Object storing data gathered by the wizard
+     */
     private WizardImportData data = new WizardImportData();
 
+    /**
+     * Reference of controller being used by this wizard
+     */
     private Controller controller;
+
+    /**
+     * Reference of model being used by this wizard
+     */
     private Model model;
 
+    /*
+     * All of the pages provided by this wizard
+     */
     private WizardImportSourcePage sourcePage;
     private WizardImportCsvPage csvPage;
     private WizardImportColumnPage columnPage;
@@ -22,8 +52,18 @@ public class WizardImport extends Wizard {
     private WizardImportTablePage tablePage;
     private WizardImportXlsPage xlsPage;
 
+    /**
+     * Holds reference to the page currently being shown
+     *
+     * This is set within {@link #getNextPage(IWizardPage)} and later used by
+     * {@link #canFinish()}.
+     */
     private IWizardPage currentPage;
 
+
+    /**
+     * Returns a reference to the controller being used by this wizard
+     */
     Controller getController()
     {
 
@@ -31,6 +71,9 @@ public class WizardImport extends Wizard {
 
     }
 
+    /**
+     * Returns a reference to the model being used by this wizard
+     */
     Model getModel()
     {
 
@@ -38,12 +81,21 @@ public class WizardImport extends Wizard {
 
     }
 
+    /**
+     * Returns a reference to the object containing the gathered data
+     */
     WizardImportData getData() {
 
         return data;
 
     }
 
+    /**
+     * Creates a new data import wizard and sets the window title
+     *
+     * @param controller Reference to controller
+     * @param model Reference to model
+     */
     public WizardImport(Controller controller, Model model)
     {
 
@@ -54,6 +106,14 @@ public class WizardImport extends Wizard {
 
     }
 
+    /**
+     * Adds pages to the wizard.
+     *
+     * @note Note that for reasons of simplicity all pages are directly added
+     * here. The page ordering is handled by {@link #getNextPage(IWizardPage)}.
+     *
+     * TODO Add pages in a more elegant way
+     */
     @Override
     public void addPages()
     {
@@ -81,6 +141,19 @@ public class WizardImport extends Wizard {
 
     }
 
+    /**
+     * Handles the correct ordering of wizard pages.
+     *
+     * As {@link #addPage(IWizardPage)} simply adds all of the wizard pages,
+     * this method makes sure that the correct one is shown next. The process
+     * flow depends upon the selected source and the <code>currentPage</code>.
+     *
+     * @param currentPage The page currently being shown
+     *
+     * @return The page that will be shown next
+     *
+     * TODO Implement in a more elegant way
+     */
     @Override
     public IWizardPage getNextPage(IWizardPage currentPage) {
 
@@ -130,6 +203,12 @@ public class WizardImport extends Wizard {
 
     }
 
+    /**
+     * Indicates whether finish button should be clickable.
+     *
+     * The wizard can only be finished after the user has seen the preview on
+     * the {@link #previewPage}.
+     */
     @Override
     public boolean canFinish() {
 
@@ -137,11 +216,14 @@ public class WizardImport extends Wizard {
 
     }
 
+    /**
+     * Gets executed once the finish button was clicked.
+     *
+     * TODO Implement actual import of data
+     */
     @Override
     public boolean performFinish()
     {
-
-        // TODO: By now everything is known to actually import the data ...
 
         return true;
 
