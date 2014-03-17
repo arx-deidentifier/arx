4ac41c858d649d4bea2247903e27f6c7cc6a9eda
Karol Babioch <karol@babioch.de>
2014-03-18 00:53:49 +0100

    gui: wizard: importdata: columnPageNameEditingSupport: Add javadoc comments

--- src/gui/org/deidentifier/arx/gui/view/impl/wizard/importdata/WizardImportColumnPageNameEditingSupport.java
+++ src/gui/org/deidentifier/arx/gui/view/impl/wizard/importdata/WizardImportColumnPageNameEditingSupport.java
@@ -6,11 +6,27 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TextCellEditor;
 
 
+/**
+ * Implements the editing support for name column within the column page
+ *
+ * This allows to change the name of columns with the column page
+ * {@link WizardImportColumnPage}. The modifications are performed within a
+ * simple text field {@link TextCellEditor}. The name itself is stored
+ * with the appropriate {@link WizardImportDataColumn} object.
+ */
 public class WizardImportColumnPageNameEditingSupport extends EditingSupport {
 
+    /**
+     * Actual editor
+     */
     private TextCellEditor editor;
 
 
+    /**
+     * Creates a new editor for the given {@link TableViewer}.
+     *
+     * @param viewer The TableViewer this editor is implemented for
+     */
     public WizardImportColumnPageNameEditingSupport(TableViewer viewer)
     {
 
@@ -20,6 +36,9 @@ public class WizardImportColumnPageNameEditingSupport extends EditingSupport {
 
     }
 
+    /**
+     * Indicate that all cells within this column can be edited
+     */
     @Override
     protected boolean canEdit(Object arg0)
     {
@@ -28,6 +47,9 @@ public class WizardImportColumnPageNameEditingSupport extends EditingSupport {
 
     }
 
+    /**
+     * Returns a reference to {@link #editor}.
+     */
     @Override
     protected CellEditor getCellEditor(Object arg0)
     {
@@ -36,6 +58,9 @@ public class WizardImportColumnPageNameEditingSupport extends EditingSupport {
 
     }
 
+    /**
+     * Gets name of column ({@link WizardImportDataColumn#getName()})
+     */
     @Override
     protected Object getValue(Object arg0)
     {
@@ -44,6 +69,9 @@ public class WizardImportColumnPageNameEditingSupport extends EditingSupport {
 
     }
 
+    /**
+     * Sets name of column ({@link WizardImportDataColumn#setName(String)})
+     */
     @Override
     protected void setValue(Object element, Object value)
     {
