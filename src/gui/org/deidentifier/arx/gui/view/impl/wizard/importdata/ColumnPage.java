3d0ec54e846c698e8f5106af737f5a1c51192aef
Karol Babioch <karol@babioch.de>
2014-03-18 20:36:49 +0100

    gui: wizard: importdata: TablePage: Move EditingSupport into class

--- src/gui/org/deidentifier/arx/gui/view/impl/wizard/importdata/ColumnPage.java
+++ src/gui/org/deidentifier/arx/gui/view/impl/wizard/importdata/ColumnPage.java
@@ -1,13 +1,21 @@
 package org.deidentifier.arx.gui.view.impl.wizard.importdata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.deidentifier.arx.DataType;
 import org.deidentifier.arx.DataType.Entry;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.ComboBoxCellEditor;
+import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
@@ -106,7 +114,7 @@ public class ColumnPage extends WizardPage {
         tblclmnEnabled.addSelectionListener(new ColumnEnabledSelectionListener());
 
         tableViewerColumnName = new TableViewerColumn(checkboxTableViewer, SWT.NONE);
-        tableViewerColumnName.setEditingSupport(new ColumnPageNameEditingSupport(checkboxTableViewer));
+        tableViewerColumnName.setEditingSupport(new NameEditingSupport(checkboxTableViewer));
         tableViewerColumnName.setLabelProvider(new ColumnLabelProvider() {
 
             @Override
@@ -127,7 +135,7 @@ public class ColumnPage extends WizardPage {
         tblclmnName.setText("Name");
 
         tableViewerColumnDatatype = new TableViewerColumn(checkboxTableViewer, SWT.NONE);
-        tableViewerColumnDatatype.setEditingSupport(new ColumnPageDatatypeEditingSupport(checkboxTableViewer));
+        tableViewerColumnDatatype.setEditingSupport(new DatatypeEditingSupport(checkboxTableViewer));
         tableViewerColumnDatatype.setLabelProvider(new ColumnLabelProvider() {
 
             @Override
@@ -212,6 +220,212 @@ public class ColumnPage extends WizardPage {
                 if (column.isEnabled()) {
 
                     setPageComplete(true);
+
+                    return;
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+    /**
+     * Implements the editing support for name column within the column page
+     *
+     * This allows to change the name of columns with the column page
+     * {@link ColumnPage}. The modifications are performed within a simple text
+     * field {@link TextCellEditor}. The name itself is stored with the
+     * appropriate {@link ImportDataColumn} object.
+     */
+    public class NameEditingSupport extends EditingSupport {
+
+        /**
+         * Actual editor
+         */
+        private TextCellEditor editor;
+
+
+        /**
+         * Creates a new editor for the given {@link TableViewer}.
+         *
+         * @param viewer The TableViewer this editor is implemented for
+         */
+        public NameEditingSupport(TableViewer viewer)
+        {
+
+            super(viewer);
+
+            editor = new TextCellEditor(viewer.getTable());
+
+        }
+
+        /**
+         * Indicate that all cells within this column can be edited
+         */
+        @Override
+        protected boolean canEdit(Object arg0)
+        {
+
+            return true;
+
+        }
+
+        /**
+         * Returns a reference to {@link #editor}.
+         */
+        @Override
+        protected CellEditor getCellEditor(Object arg0)
+        {
+
+            return editor;
+
+        }
+
+        /**
+         * Gets name of column ({@link ImportDataColumn#getName()})
+         */
+        @Override
+        protected Object getValue(Object arg0)
+        {
+
+            return ((ImportDataColumn)arg0).getName();
+
+        }
+
+        /**
+         * Sets name of column ({@link ImportDataColumn#setName(String)})
+         */
+        @Override
+        protected void setValue(Object element, Object value)
+        {
+
+            ((ImportDataColumn)element).setName((String)value);
+            getViewer().update(element, null);
+
+        }
+
+    }
+
+
+    /**
+     * Implements editing support for datatype column within the column page
+     *
+     * This allows to change the datatype of columns with the column page
+     * {@link ColumnPage}. The modifications are performed with a combo box
+     * {@link ComboBoxCellEditor}. The datatype itself is stored with the
+     * appropriate {@link ImportDataColumn} object.
+     *
+     * TODO Implement better editor for datatypes of arx framework
+     */
+    public class DatatypeEditingSupport extends EditingSupport {
+
+        /**
+         * Actual editor
+         */
+        private ComboBoxCellEditor editor;
+
+        /**
+         * Allowed values for the user to choose from
+         */
+        private String[] choices;
+
+
+        /**
+         * Creates a new editor for the given {@link TableViewer}.
+         *
+         * @param viewer The TableViewer this editor is implemented for
+         */
+        public DatatypeEditingSupport(TableViewer viewer)
+        {
+
+            super(viewer);
+
+            List<String> labels = new ArrayList<String>();
+
+            for (Entry<?> entry : DataType.LIST) {
+
+                labels.add(entry.getLabel());
+
+            }
+
+            choices = labels.toArray(new String[labels.size()]);
+
+            editor = new ComboBoxCellEditor(viewer.getTable(), choices, SWT.READ_ONLY);
+
+        }
+
+        /**
+         * Indicate that all cells within this column can be edited
+         */
+        @Override
+        protected boolean canEdit(Object arg0)
+        {
+
+            return true;
+
+        }
+
+        /**
+         * Returns a reference to {@link #editor}.
+         */
+        @Override
+        protected CellEditor getCellEditor(Object arg0)
+        {
+
+            return editor;
+
+        }
+
+        /**
+         * Returns index of datatype for given column
+         */
+        @Override
+        protected Object getValue(Object element)
+        {
+
+            DataType<?> datatype = ((ImportDataColumn)element).getDatatype();
+
+            int i = 0;
+
+            for (Entry<?> entry : DataType.LIST) {
+
+                if (entry.newInstance().getClass() == datatype.getClass()) {
+
+                    return i;
+
+                }
+
+                i++;
+
+            }
+
+            return null;
+
+
+        }
+
+        /**
+         * Sets new datatype for given column
+         *
+         * Internally this function makes use of
+         * {@link ImportDataColumn#setDatatype(Class)}. The values itself are
+         * taken from {@link #choices}.
+         */
+        @Override
+        protected void setValue(Object element, Object value)
+        {
+
+            String label = choices[(int) value];
+
+            for (Entry<?> entry : DataType.LIST) {
+
+                if (entry.getLabel().equals(label)) {
+
+                    ((ImportDataColumn)element).setDatatype(entry.newInstance());
+                    getViewer().update(element, null);
 
                     return;
 
