461d07873c14b44c98db91cb14aafdd94aeb3849
Fabian Prasser <prasser@in.tum.de>
2014-07-01 18:19:43 +0200

    Add example for using the NDS metric

---
+++ src/example/org/deidentifier/arx/examples/Example14.java
@@ -0,0 +1,117 @@
+/*
+ * ARX: Efficient, Stable and Optimal Data Anonymization
+ * Copyright (C) 2012 - 2014 Florian Kohlmayer, Fabian Prasser
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.deidentifier.arx.examples;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.deidentifier.arx.ARXAnonymizer;
+import org.deidentifier.arx.ARXConfiguration;
+import org.deidentifier.arx.ARXResult;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+import org.deidentifier.arx.AttributeType.Hierarchy.DefaultHierarchy;
+import org.deidentifier.arx.Data;
+import org.deidentifier.arx.Data.DefaultData;
+import org.deidentifier.arx.criteria.KAnonymity;
+import org.deidentifier.arx.metric.Metric;
+
+/**
+ * This class implements an example on how to use the NDS Metric
+ * 
+ * @author Fabian Prasser
+ * @author Florian Kohlmayer
+ */
+public class Example14 extends Example {
+
+    /**
+     * Entry point.
+     * 
+     * @param args
+     *            the arguments
+     */
+    public static void main(final String[] args) {
+
+        // Define data
+        final DefaultData data = Data.create();
+        data.add("age", "gender", "zipcode");
+        data.add("34", "male", "81667");
+        data.add("45", "female", "81675");
+        data.add("66", "male", "81925");
+        data.add("70", "female", "81931");
+        data.add("34", "female", "81931");
+        data.add("70", "male", "81931");
+        data.add("45", "male", "81931");
+
+        // Define hierarchies
+        final DefaultHierarchy age = Hierarchy.create();
+        age.add("34", "<50", "*");
+        age.add("45", "<50", "*");
+        age.add("66", ">=50", "*");
+        age.add("70", ">=50", "*");
+
+        final DefaultHierarchy gender = Hierarchy.create();
+        gender.add("male", "*");
+        gender.add("female", "*");
+
+        // Only excerpts for readability
+        final DefaultHierarchy zipcode = Hierarchy.create();
+        zipcode.add("81667", "8166*", "816**", "81***", "8****", "*****");
+        zipcode.add("81675", "8167*", "816**", "81***", "8****", "*****");
+        zipcode.add("81925", "8192*", "819**", "81***", "8****", "*****");
+        zipcode.add("81931", "8193*", "819**", "81***", "8****", "*****");
+
+        data.getDefinition().setAttributeType("age", age);
+        data.getDefinition().setAttributeType("gender", gender);
+        data.getDefinition().setAttributeType("zipcode", zipcode);
+
+        // Create an instance of the anonymizer
+        final ARXAnonymizer anonymizer = new ARXAnonymizer();
+        final ARXConfiguration config = ARXConfiguration.create();
+        config.addCriterion(new KAnonymity(3));
+        
+        // NDS-specific settings
+        config.setMaxOutliers(1d); // Recommended default: 1d
+        config.setAttributeWeight("age", 0.5d); // attribute weight
+        config.setAttributeWeight("gender", 0.3d); // attribute weight
+        config.setAttributeWeight("zipcode", 0.5d); // attribute weight
+        config.setMetric(Metric.createNDSMetric(0.5d)); // suppression/generalization-factor
+        
+        try {
+            final ARXResult result = anonymizer.anonymize(data, config);
+
+            // Print info
+            printResult(result, data);
+
+            // Process results
+            System.out.println(" - Transformed data:");
+            final Iterator<String[]> transformed = result.getOutput(false)
+                                                         .iterator();
+            while (transformed.hasNext()) {
+                System.out.print("   ");
+                System.out.println(Arrays.toString(transformed.next()));
+            }
+
+        } catch (final IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
