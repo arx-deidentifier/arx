974b537b4d144e126fa499bd538698af78cbf61f
Florian Kohlmayer <kohlmayer@gmail.com>
2014-08-08 16:25:43 +0200

    Do not prune based on monotonic sub-metrics if practical monotonicity is assumed

--- src/main/org/deidentifier/arx/algorithm/FLASHAlgorithmImpl.java
+++ src/main/org/deidentifier/arx/algorithm/FLASHAlgorithmImpl.java
@@ -357,7 +357,8 @@ public class FLASHAlgorithmImpl extends AbstractAlgorithm {
         }
         
         // Check, if we can prune based on a monotonic sub-metric
-        if (getGlobalOptimum() != null) {
+        if (!checker.getConfiguration().isPracticalMonotonicity() && 
+            getGlobalOptimum() != null) {
             
             // We skip, if we already know that this node has insufficient utility
             if (node.hasProperty(Node.PROPERTY_INSUFFICIENT_UTILITY)) {
