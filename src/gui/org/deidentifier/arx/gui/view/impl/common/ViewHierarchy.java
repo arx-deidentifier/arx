8518abe4110bbfe1c9bfc2b822e0c2d59bda48a2
Florian Kohlmayer <kohlmayer@gmail.com>
2014-11-25 16:34:03 +0100

    Correctly check for empty hierarchies

--- src/gui/org/deidentifier/arx/gui/view/impl/common/ViewHierarchy.java
+++ src/gui/org/deidentifier/arx/gui/view/impl/common/ViewHierarchy.java
@@ -901,7 +901,7 @@ public class ViewHierarchy implements IView {
         final List<String> minItems = new ArrayList<String>();
         final List<String> maxItems = new ArrayList<String>();
         minItems.add(ITEM_ALL);
-        for (int i = 0; i < (hierarchy==null ? 0 : hierarchy[0].length); i++) {
+        for (int i = 0; i < (hierarchy==null || hierarchy.length == 0 ? 0 : hierarchy[0].length); i++) {
             minItems.add(String.valueOf(i));
             maxItems.add(String.valueOf(i));
         }
