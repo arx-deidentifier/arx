7f3bfd3e7c94cdc9a5110c7e411b32bed2529bd6
Florian Kohlmayer <kohlmayer@gmail.com>
2015-02-03 18:31:38 +0100

    Code formatting

--- src/gui/org/deidentifier/arx/gui/view/impl/explore/ViewList.java
+++ src/gui/org/deidentifier/arx/gui/view/impl/explore/ViewList.java
@@ -277,7 +277,6 @@ public class ViewList extends ViewSolutionSpace {
                 };
                 table.addListener(SWT.SetData, listener);
                 table.setItemCount(list.size());
-                
                 table.setRedraw(true);
             }
         });
