11b7ec19df0d5916e24f35f9fa87d4e78b625177
Florian Kohlmayer <kohlmayer@gmail.com>
2015-02-06 14:05:49 +0100

    Add risk estimation test cases

--- src/test/org/deidentifier/arx/test/TestRiskMetrics.java
+++ src/test/org/deidentifier/arx/test/TestRiskMetrics.java
@@ -17,68 +17,118 @@
 
 package org.deidentifier.arx.test;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import junit.framework.TestCase;
 
 import org.deidentifier.arx.ARXAnonymizer;
 import org.deidentifier.arx.ARXConfiguration;
 import org.deidentifier.arx.ARXResult;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+import org.deidentifier.arx.Data;
 import org.deidentifier.arx.DataHandle;
-import org.deidentifier.arx.criteria.DPresence;
 import org.deidentifier.arx.criteria.KAnonymity;
+import org.deidentifier.arx.io.CSVHierarchyInput;
 import org.deidentifier.arx.risk.RiskEstimator;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Test for risk metrics
+ * Test for risk metrics.
  *
  * @author Fabian Prasser
  * @author Florian Kohlmayer
  */
 public class TestRiskMetrics extends TestCase {
 
-    @Test
-    public void testHighestIndividualRisk() {
-        DataProvider provider = new DataProvider();
-        provider.createDataDefinition();
-        DataHandle handle = provider.getData().getHandle();
+    /**
+     * Returns the data object for a given dataset
+     *
+     * @param dataset the dataset
+     * @return the data object
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    private static Data getDataObject(final String dataset) throws IOException {
 
-        RiskEstimator estimator = new RiskEstimator(handle);
-        double risk = estimator.getHighestIndividualRisk();
+        final Data data = Data.create(dataset, ';');
 
-        // Risk before anonymization
-        assertTrue(risk == 1.0d);
+        // Read generalization hierachies
+        final FilenameFilter hierarchyFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(final File dir, final String name) {
+                if (name.matches(dataset.substring(dataset.lastIndexOf("/") + 1, dataset.length() - 4) +
+                                 "_hierarchy_(.)+.csv")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
 
-        final ARXAnonymizer anonymizer = new ARXAnonymizer();
-        final ARXConfiguration config = ARXConfiguration.create();
-        config.addCriterion(new KAnonymity(2));
-        config.setMaxOutliers(0d);
+        final File testDir = new File(dataset.substring(0, dataset.lastIndexOf("/")));
+        final File[] genHierFiles = testDir.listFiles(hierarchyFilter);
+        final Pattern pattern = Pattern.compile("_hierarchy_(.*?).csv");
 
-        ARXResult result = null;
-        try {
-            result = anonymizer.anonymize(provider.getData(), config);
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (final File file : genHierFiles) {
+            final Matcher matcher = pattern.matcher(file.getName());
+            if (matcher.find()) {
+
+                final CSVHierarchyInput hier = new CSVHierarchyInput(file, ';');
+                final String attributeName = matcher.group(1);
+
+                // use all found attribute hierarchies as qis
+                data.getDefinition().setAttributeType(attributeName, Hierarchy.create(hier.getHierarchy()));
+
+            }
         }
-        final DataHandle outHandle = result.getOutput(false);
 
-        RiskEstimator estimator2 = new RiskEstimator(outHandle);
-        double riskAnon = estimator2.getHighestIndividualRisk();
-        // Risk after anonymization
-        assertTrue(riskAnon == 0.5d);
+        return data;
     }
 
+    /**
+     * Test average risk using the example dataset.
+     */
     @Test
     public void testAverageRisk() {
         DataProvider provider = new DataProvider();
         provider.createDataDefinition();
+        // Risk before anonymization
+        assertTrue(getAverageRisk(provider.getData().getHandle()) == 1.0d);
+
+        // Risk after anonymization
+        assertTrue(getAverageRisk(getAnonymizedData(provider.getData())) == 0.42857142857142855d);
+    }
+
+    /**
+     * Test average risk using the adult dataset.
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void testAverageRisk2() throws IOException {
+        Data data = getDataObject("../arx-data/data-junit/adult.csv");
+        // Risk before anonymization
+        assertTrue(getAverageRisk(data.getHandle()) == 0.6465751607983555d);
+
+        // Risk after anonymization
+        assertTrue(getAverageRisk(getAnonymizedData(data)) == 0.001922949406538028);
+    }
+
+    /**
+     * Test decision rule using the test dataset.
+     */
+    @Test
+    public void testDecisionRule() {
+        DataProvider provider = new DataProvider();
+        provider.createDataDefinition();
         DataHandle handle = provider.getData().getHandle();
 
         RiskEstimator estimator = new RiskEstimator(handle);
-        double risk = estimator.getEquivalenceClassRisk();
+        double risk = estimator.getPopulationUniquesRisk();
 
         // Risk before anonymization
         assertTrue(risk == 1.0d);
@@ -97,8 +147,121 @@ public class TestRiskMetrics extends TestCase {
         final DataHandle outHandle = result.getOutput(false);
 
         RiskEstimator estimator2 = new RiskEstimator(outHandle);
-        double riskAnon = estimator2.getEquivalenceClassRisk();
-        // Risk after anonymization
-        assertTrue(riskAnon == 0.42857142857142855d);
+        try {
+            estimator2.getPopulationUniquesRisk();
+        } catch (final IllegalStateException e) {
+            return;
+        }
+        Assert.fail();
+
     }
+
+    /**
+     * Test decision rule using the adult dataset.
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void testDecissionRule2() throws IOException {
+
+        Data data = getDataObject("../arx-data/data-junit/adult.csv");
+        DataHandle handle = data.getHandle();
+
+        RiskEstimator estimator = new RiskEstimator(handle);
+        double risk = estimator.getPopulationUniquesRisk();
+
+        assertTrue(risk == 0.2768499388383177d);
+
+        estimator = new RiskEstimator(handle, 0.2);
+        risk = estimator.getPopulationUniquesRisk();
+
+        assertTrue(risk == 0.3577099234829125d);
+
+        estimator = new RiskEstimator(handle, 0.01);
+        risk = estimator.getPopulationUniquesRisk();
+
+        assertTrue(risk == 0.14460835311745487d);
+
+        estimator = new RiskEstimator(handle, 1);
+        risk = estimator.getPopulationUniquesRisk();
+
+        assertTrue(risk == 0.5142895033485844d);
+
+    }
+
+    /**
+     * Test highest individual risk using the test dataset.
+     */
+    @Test
+    public void testHighestIndividualRisk() {
+        DataProvider provider = new DataProvider();
+        provider.createDataDefinition();
+        // Risk before anonymization
+        assertTrue(getHighestIndividualRisk(provider.getData().getHandle()) == 1.0d);
+
+        // Risk after anonymization
+        assertTrue(getHighestIndividualRisk(getAnonymizedData(provider.getData())) == 0.5d);
+    }
+
+    /**
+     * Test highest individual risk using the adult dataset.
+     *
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    @Test
+    public void testHighestIndividualRisk2() throws IOException {
+        Data data = getDataObject("../arx-data/data-junit/adult.csv");
+        // Risk before anonymization
+        assertTrue(getHighestIndividualRisk(data.getHandle()) == 1.0d);
+
+        // Risk after anonymization
+        assertTrue(getHighestIndividualRisk(getAnonymizedData(data)) == 0.5d);
+    }
+
+    /**
+     * 2-Anonymizes the given data. No suppression allowed.
+     *
+     * @param data the data
+     * @return the anonymized data
+     */
+    private DataHandle getAnonymizedData(Data data) {
+        final ARXAnonymizer anonymizer = new ARXAnonymizer();
+        final ARXConfiguration config = ARXConfiguration.create();
+        config.addCriterion(new KAnonymity(2));
+        config.setMaxOutliers(0d);
+
+        ARXResult result = null;
+        try {
+            result = anonymizer.anonymize(data, config);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        final DataHandle outHandle = result.getOutput(false);
+        return outHandle;
+    }
+
+    /**
+     * Gets the average risk.
+     *
+     * @param handle the handle
+     * @return the average risk
+     */
+    private double getAverageRisk(DataHandle handle) {
+        RiskEstimator estimator = new RiskEstimator(handle);
+        double risk = estimator.getEquivalenceClassRisk();
+        return risk;
+    }
+
+    /**
+     * Gets the highest individual risk.
+     *
+     * @param handle the handle
+     * @return the highest individual risk
+     */
+    private double getHighestIndividualRisk(DataHandle handle) {
+        RiskEstimator estimator = new RiskEstimator(handle);
+        double risk = estimator.getHighestIndividualRisk();
+        return risk;
+    }
+
 }
