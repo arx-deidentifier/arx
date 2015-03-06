1d84c6ca100df1fd3f1fc5225ba97cdc66bbb860
Florian Kohlmayer <kohlmayer@gmail.com>
2015-03-06 23:17:58 +0100

    Use port of the The Lightspeed Matlab toolbox for digamma and trigamma

--- src/main/org/deidentifier/arx/risk/Gamma.java
+++ src/main/org/deidentifier/arx/risk/Gamma.java
@@ -23,145 +23,166 @@ package org.deidentifier.arx.risk;
  * @author Fabian Prasser
  */
 public class Gamma {
-
-    /** The Constant LARGE. */
-    private final static double LARGE = 9.5;
-
-    /** The Constant D1. */
-    private final static double D1    = -0.5772156649015328606065121; // digamma(1)
-
-    /** The Constant D2. */
-    private final static double D2    = Math.pow(Math.PI, 2.0) / 6.0;
-
-    /** The Constant SMALL. */
-    private final static double SMALL = 1e-6;
-
-    /** The Constant S3. */
-    private final static double S3    = 1.0 / 12.0;
-
-    /** The Constant S4. */
-    private final static double S4    = 1.0 / 120.0;
-
-    /** The Constant S5. */
-    private final static double S5    = 1.0 / 252.0;
-
-    /** The Constant S6. */
-    private final static double S6    = 1.0 / 240.0;
-
-    /** The Constant S7. */
-    private final static double S7    = 1.0 / 132.0;
-
-    /** The Constant S8. */
-    private final static double S8    = 1.0 / 30.0;
-
-    /** The Constant S9. */
-    private final static double S9    = 1.0 / 6.0;
-
-    /** The Constant S10. */
-    private final static double S10   = 1.0 / 42.0;
-
-    /** The Constant S11. */
-    private final static double S11   = 1.0 / 13.2;
-
-    /** The Constant S12. */
-    private final static double S12   = 1.0 / 2.0;
+    
     /**
      * Approximates the digamma function.
-     * Adapted from: https://github.com/georgg/georg/blob/master/src/old%20code/SpecialMathFn/Digamma.java
-     * which in turn is adapted from Tony Minka's Lightspeed 2.0 Matlab library
-     * 
-     * DIGAMMA(X) returns digamma(x) = d log(gamma(x)) / dx
-     * If X is a matrix, returns the digamma function evaluated at each element.
-     * 
-     * Reference:
-     * 
-     * J Bernardo,
-     * Psi ( Digamma ) Function,
-     * Algorithm AS 103,
-     * Applied Statistics,
-     * Volume 25, Number 3, pages 315-317, 1976.
-     * From http://www.psc.edu/~burkardt/src/dirichlet/dirichlet.f
-     *
+     * Java port of the "The Lightspeed Matlab toolbox" version 2.7 by Tom Minka
+     * see: http://research.microsoft.com/en-us/um/people/minka/software/lightspeed/
      * @param x input value
      * @return approximation of digamma for x
      */
     public static double digamma(double x) {
         
-        double y = 0.0d;
-        double r = 0.0d;
-
+        /* Illegal arguments */
         if (Double.isInfinite(x) || Double.isNaN(x)) {
-            return 0.0d / 0.0d;
+            return Double.NaN;
         }
-
+        
+        /* Singularities */
         if (x == 0.0d) {
-            return -1.0d / 0.0d;
+            return Double.NEGATIVE_INFINITY;
         }
-
+        
+        /* Negative values */
+        /*
+         * Use the reflection formula (Jeffrey 11.1.6):
+         * digamma(-x) = digamma(x+1) + pi*cot(pi*x)
+         * 
+         * This is related to the identity
+         * digamma(-x) = digamma(x+1) - digamma(z) + digamma(1-z)
+         * where z is the fractional part of x
+         * For example:
+         * digamma(-3.1) = 1/3.1 + 1/2.1 + 1/1.1 + 1/0.1 + digamma(1-0.1)
+         * = digamma(4.1) - digamma(0.1) + digamma(1-0.1)
+         * Then we use
+         * digamma(1-z) - digamma(z) = pi*cot(pi*z)
+         */
         if (x < 0.0d) {
-            // Use the reflection formula (Jeffrey 11.1.6):
-            // digamma(-x) = digamma(x+1) + pi*cot(pi*x)
-            y = digamma(-x + 1.0d) + (Math.PI * (1.0d / Math.tan(-Math.PI * x)));
-            return y;
-            // This is related to the identity
-            // digamma(-x) = digamma(x+1) - digamma(z) + digamma(1-z)
-            // where z is the fractional part of x
-            // For example:
-            // digamma(-3.1) = 1/3.1 + 1/2.1 + 1/1.1 + 1/0.1 + digamma(1-0.1)
-            // = digamma(4.1) - digamma(0.1) + digamma(1-0.1)
-            // Then we use
-            // digamma(1-z) - digamma(z) = pi*cot(pi*z)
+            return digamma(1.0d - x) + (StrictMath.PI / StrictMath.tan(-StrictMath.PI * x));
         }
-
-        // Use approximation if argument <= small.
-        if (x <= SMALL) {
-            y = ((y + D1) - (1.0d / x)) + (D2 * x);
-            return y;
+        
+        /* Use Taylor series if argument <= small */
+        if (x <= SMALL_DIGAMMA) {
+            return (DIGAMMA_1 - (1.0d / x)) + (TRIGAMMA_1 * x);
         }
-
-        // Reduce to digamma(X + N) where (X + N) >= large.
-        while (true) {
-            if ((x > SMALL) && (x < LARGE)) {
-                y = y - (1.0d / x);
-                x = x + 1.0d;
-            } else {
-                break;
-            }
+        
+        double result = 0.0d;
+        /* Reduce to digamma(X + N) where (X + N) >= large */
+        while (x < LARGE_DIGAMMA) {
+            result -= 1.0d / x;
+            x++;
         }
-
-        // Use de Moivre's expansion if argument >= large.
-        // In maple: asympt(Psi(x), x);
-        if (x >= LARGE) {
-            r = 1.0d / x;
-            y = (y + Math.log(x)) - (0.5d * r);
-            r = r * r;
-            y = y - (r * (S3 - (r * (S4 - (r * (S5 - (r * (S6 - (r * S7)))))))));
+        
+        /* Use de Moivre's expansion if argument >= C */
+        /* This expansion can be computed in Maple via asympt(Psi(x),x) */
+        if (x >= LARGE_DIGAMMA) {
+            double r = 1.0d / x;
+            result += StrictMath.log(x) - (0.5d * r);
+            r *= r;
+            result -= r * (S3 - (r * (S4 - (r * (S5 - (r * (S6 - (r * S7))))))));
         }
-
-        return y;
+        
+        return result;
     }
-
+    
     /**
      * Approximates the trigamma function.
-     * Adapted from: https://github.com/lintool/Cloud9/blob/master/src/main/java/edu/umd/cloud9/math/Gamma.java
-     * 
-     * Author Mihai Preda, 2006.
-     * The author disclaims copyright to this source code.
-     *
+     * Java port of the "The Lightspeed Matlab toolbox" version 2.7 by Tom Minka
+     * see: http://research.microsoft.com/en-us/um/people/minka/software/lightspeed/
      * @param x input value
      * @return approximation of trigamma for x
      */
     public static double trigamma(double x) {
-        double p = 0d;
-
-        x = x + 6.0d;
-        p = 1.0d / (x * x);
-        p = (((((((((((S11 * p) - S8) * p) + S10) * p) - S8) * p) + S9) * p) + 1d) / x) + (S12 * p);
-        for (int i = 0; i < 6; i++) {
-            x = x - 1.0d;
-            p = (1.0d / (x * x)) + p;
+        /* Illegal arguments */
+        if (Double.isInfinite(x) || Double.isNaN(x)) {
+            return Double.NaN;
         }
-
-        return p;
+        
+        /* Singularities */
+        if (x == 0.0d) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        
+        /* Negative values */
+        /*
+         * Use the derivative of the digamma reflection formula:
+         * -trigamma(-x) = trigamma(x+1) - (pi*csc(pi*x))^2
+         */
+        if (x < 0.0d) {
+            double r = StrictMath.PI / StrictMath.sin(-StrictMath.PI * x);
+            return -trigamma(1.0d - x) + (r * r);
+        }
+        
+        /* Use Taylor series if argument <= small */
+        if (x <= SMALL_TRIGAMMA) {
+            return (1.0d / (x * x)) + TRIGAMMA_1 + (TETRAGAMMA_1 * x);
+        }
+        
+        double result = 0.0d;
+        /* Reduce to trigamma(x+n) where ( X + N ) >= B */
+        while (x < LARGE_TRIGAMMA) {
+            result += 1.0d / (x * x);
+            x++;
+        }
+        
+        /* Apply asymptotic formula when X >= B */
+        /* This expansion can be computed in Maple via asympt(Psi(1,x),x) */
+        if (x >= LARGE_DIGAMMA) {
+            double r = 1.0d / (x * x);
+            result += (0.5d * r) + ((1.0d + (r * (B2 + (r * (B4 + (r * (B6 + (r * (B8 + (r * B10)))))))))) / x);
+        }
+        return result;
     }
+    
+    /** The Constant LARGE_DIGAMMA. */
+    private final static double LARGE_DIGAMMA  = 12.0;
+    
+    /** The Constant LARGE_TRIGAMMA. */
+    private final static double LARGE_TRIGAMMA = 8.0;
+    
+    /** The Constant DIGAMMA_1. */
+    private final static double DIGAMMA_1      = -0.57721566490153286060651209008240243104215933593992d;  // -digamma(1) = Euler–Mascheroni constant
+                                                                                                           
+    /** The Constant TRIGAMMA_1. */
+    private final static double TRIGAMMA_1     = (StrictMath.PI * StrictMath.PI) / 6.0;                   // trigamma(1) = pi^2/6 = Zeta(2)
+                                                                                                           
+    /** The Constant TETRAGAMMA_1. */
+    private final static double TETRAGAMMA_1   = -2.0d * 1.202056903159594285399738161511449990764986292d; // -2 * Zeta(3) = -2 * Apéry's constant
+                                                                                                           
+    /** The Constant SMALL_DIGAMMA. */
+    private final static double SMALL_DIGAMMA  = 1e-6;
+    
+    /** The Constant SMALL_TRIGAMMA. */
+    private final static double SMALL_TRIGAMMA = 1e-4;
+    
+    /** The Constant S3. */
+    private final static double S3             = 1.0 / 12.0;
+    
+    /** The Constant S4. */
+    private final static double S4             = 1.0 / 120.0;
+    
+    /** The Constant S5. */
+    private final static double S5             = 1.0 / 252.0;
+    
+    /** The Constant S6. */
+    private final static double S6             = 1.0 / 240.0;
+    
+    /** The Constant S7. */
+    private final static double S7             = 1.0 / 132.0;
+    
+    /** The Constant B2. */
+    private final static double B2             = 1.0 / 6.0;
+    
+    /** The Constant B4. */
+    private final static double B4             = -1.0 / 30.0;
+    
+    /** The Constant B6. */
+    private final static double B6             = 1.0 / 42.0;
+    
+    /** The Constant B8. */
+    private final static double B8             = -1.0 / 30.0;
+    
+    /** The Constant B10. */
+    private final static double B10            = 5.0 / 66.0;
+    
 }
