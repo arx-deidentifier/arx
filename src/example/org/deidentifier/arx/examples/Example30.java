a99a739bd07edcb69fd350f4450480c77d56395c
Fabian Prasser <mail@fabian-prasser.de>
2015-04-09 11:09:34 +0200

    Extend example for accessing summary statistics

--- src/example/org/deidentifier/arx/examples/Example30.java
+++ src/example/org/deidentifier/arx/examples/Example30.java
@@ -17,9 +17,13 @@
 
 package org.deidentifier.arx.examples;
 
+import java.util.Date;
+import java.util.Map;
+
 import org.deidentifier.arx.Data;
 import org.deidentifier.arx.Data.DefaultData;
 import org.deidentifier.arx.DataType;
+import org.deidentifier.arx.aggregates.StatisticsSummary;
 
 /**
  * This class implements an example of how to compute summary statistics
@@ -35,6 +39,7 @@ public class Example30 extends Example {
      * @param args
      *            the arguments
      */
+    @SuppressWarnings("unchecked")
     public static void main(final String[] args) {
 
         // Define data
@@ -47,13 +52,64 @@ public class Example30 extends Example {
         data.add("34", "female", null, "05.01.1982");
         data.add("70", "male", "81931", "24.03.1982");
         data.add("45", "male", "81931", "NULL");
-        
+
+        // Print everything
+        System.out.println("***************************");
+        System.out.println("* Dumping the whole object*");
+        System.out.println("***************************");
         System.out.println(data.getHandle().getStatistics().getSummaryStatistics(true));
         
+        // Alter definition
         data.getDefinition().setDataType("age", DataType.INTEGER);
         data.getDefinition().setDataType("zipcode", DataType.DECIMAL);
         data.getDefinition().setDataType("date", DataType.DATE);
 
+        // Print everything
+        System.out.println("");
+        System.out.println("***************************");
+        System.out.println("* Dumping the whole object*");
+        System.out.println("***************************");
         System.out.println(data.getHandle().getStatistics().getSummaryStatistics(true));
+        
+        // Access individual measures
+        Map<String, StatisticsSummary<?>> statistics = data.getHandle().getStatistics().getSummaryStatistics(true);
+        
+        // For age
+        System.out.println("");
+        System.out.println("***************************");
+        System.out.println("* Individual statistics   *");
+        System.out.println("***************************");
+        StatisticsSummary<Long> statisticsAge = (StatisticsSummary<Long>)statistics.get("age");
+        if (statisticsAge.isGeometricMeanAvailable()) {
+            System.out.println("Geometric mean of age");
+            System.out.println(" - As double: " + statisticsAge.getGeometricMeanAsDouble());
+            System.out.println(" - As value : " + statisticsAge.getGeometricMeanAsValue());
+            System.out.println(" - As string: " + statisticsAge.getGeometricMeanAsString());
+        }
+        
+        // For date
+        System.out.println("");
+        System.out.println("***************************");
+        System.out.println("* Individual statistics   *");
+        System.out.println("***************************");
+        StatisticsSummary<Date> statisticsDate = (StatisticsSummary<Date>)statistics.get("date");
+        if (statisticsDate.isSampleVarianceAvailable()) {
+            System.out.println("Sample variance of date");
+            System.out.println(" - As double: " + statisticsDate.getSampleVarianceAsDouble());
+            System.out.println(" - As value : " + statisticsDate.getSampleVarianceAsValue());
+            System.out.println(" - As string: " + statisticsDate.getSampleVarianceAsString());
+        }
+        if (statisticsDate.isArithmeticMeanAvailable()) {
+            System.out.println("Arithmetic mean of date");
+            System.out.println(" - As double: " + statisticsDate.getArithmeticMeanAsDouble());
+            System.out.println(" - As value : " + statisticsDate.getArithmeticMeanAsValue());
+            System.out.println(" - As string: " + statisticsDate.getArithmeticMeanAsString());
+        }
+        if (statisticsDate.isRangeAvailable()) {
+            System.out.println("Range of date");
+            System.out.println(" - As double: " + statisticsDate.getRangeAsDouble());
+            System.out.println(" - As value : " + statisticsDate.getRangeAsValue());
+            System.out.println(" - As string: " + statisticsDate.getRangeAsString());
+        }
     }
 }
