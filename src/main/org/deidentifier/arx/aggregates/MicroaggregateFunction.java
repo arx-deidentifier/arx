7d9545158b3ba045add55fa927655bffbca8f5cb
Florian Kohlmayer <kohlmayer@gmail.com>
2015-04-17 10:31:04 +0200

    Add documentation

--- src/main/org/deidentifier/arx/aggregates/MicroaggregateFunction.java
+++ src/main/org/deidentifier/arx/aggregates/MicroaggregateFunction.java
@@ -32,17 +32,31 @@ import org.deidentifier.arx.framework.check.distribution.Distribution;
  */
 public abstract class MicroaggregateFunction implements Serializable {
     
+    /**
+     * This class calculates the arithmetic mean for a given distribution.
+     * @author Florian Kohlmayer
+     *
+     */
     public static class ArithmeticMean extends MicroaggregateFunction {
         
-        /** SVUID */
+        /** SVUID. */
         private static final long           serialVersionUID = 8108686651029571643L;
         
+        /** Commons math object to calculate the statistic. */
         private final DescriptiveStatistics stats;
         
+        /**
+         * Instantiates.
+         */
         public ArithmeticMean() {
             this(HandlingOfNullValues.IGNORE);
         }
         
+        /**
+         * Instantiates.
+         *
+         * @param nullValueHandling the null value handling
+         */
         public ArithmeticMean(HandlingOfNullValues nullValueHandling) {
             super(nullValueHandling);
             stats = new DescriptiveStatistics();
@@ -85,17 +99,32 @@ public abstract class MicroaggregateFunction implements Serializable {
         }
     }
     
+    /**
+     * This class calculates the geometric mean for a given distribution.
+     * 
+     * @author Florian Kohlmayer
+     *
+     */
     public static class GeometricMean extends MicroaggregateFunction {
         
-        /** SVUID */
+        /** SVUID. */
         private static final long           serialVersionUID = -6715484856330698691L;
         
+        /** Commons math object to calculate the statistic. */
         private final DescriptiveStatistics stats;
         
+        /**
+         * Instantiates.
+         */
         public GeometricMean() {
             this(HandlingOfNullValues.IGNORE);
         }
         
+        /**
+         * Instantiates.
+         *
+         * @param nullValueHandling the null value handling
+         */
         public GeometricMean(HandlingOfNullValues nullValueHandling) {
             super(nullValueHandling);
             stats = new DescriptiveStatistics();
@@ -138,24 +167,50 @@ public abstract class MicroaggregateFunction implements Serializable {
         }
     }
     
+    /**
+     * Options to handle NULL values.
+     */
     public static enum HandlingOfNullValues {
         IGNORE,
         IDENTITIY
     }
     
-
-    
+    /**
+     * Double iterator for distributions.
+     * @author Florian Kohlmayer
+     *
+     */
     private class DistributionIteratorDouble implements Iterator<Double> {
         
+        /** The distribution. */
         private final Distribution           values;
+        
+        /** The data type */
         @SuppressWarnings("rawtypes")
         private final DataTypeWithRatioScale type;
+        
+        /** The dictionary. */
         private final String[]               dictionary;
+        
+        /** The index of the next bucket. */
         private int                          nextBucket;
+        
+        /** The frequency of the current value. */
         private int                          currentFrequency;
+        
+        /** The value of the current bucket. */
         private Double                       currentValue;
+        
+        /** Counts the remaining entries. */
         private int                          remaining;
         
+        /**
+         * Instantiates the iterator.
+         *
+         * @param values the values
+         * @param type the type
+         * @param dictionary the dictionary
+         */
         public DistributionIteratorDouble(Distribution values, DataTypeWithRatioScale<?> type, String[] dictionary) {
             this.values = values;
             this.type = type;
@@ -206,30 +261,39 @@ public abstract class MicroaggregateFunction implements Serializable {
         
     }
     
+    /** The NULL value handling. */
     protected HandlingOfNullValues handleNullValues = HandlingOfNullValues.IGNORE;
     
-    /** SVUID */
+    /** SVUID. */
     private static final long      serialVersionUID = 331877806010996154L;
+    
     /** The data type. */
     protected DataType<?>          type;
     
-    /** The dictionary */
+    /** The dictionary. */
     protected String[]             dictionary;
     
+    /**
+     * Instantiates a new Microaggregation.
+     */
     public MicroaggregateFunction() {
         this(HandlingOfNullValues.IGNORE);
     }
     
+    /**
+     * Instantiates a new Microaggregation.
+     *
+     * @param nullValueHandling the null value handling
+     */
     public MicroaggregateFunction(HandlingOfNullValues nullValueHandling) {
         handleNullValues = nullValueHandling;
     }
     
     /**
-     * 
      * This function returns an aggregate value.
-     * 
-     * @param values
-     * @return
+     *
+     * @param values the values
+     * @return the string
      */
     public String aggregate(Distribution values) {
         String returnValue = aggregateInternal(values);
@@ -237,16 +301,17 @@ public abstract class MicroaggregateFunction implements Serializable {
     }
     
     /**
-     * Returns the scale which is needed
-     * @return
+     * Returns the scale which is needed.
+     *
+     * @return the minimal required scale
      */
     public abstract ScaleOfMeasure getMinimalRequiredScale();
     
     /**
      * Inits the aggregate function and sets the according dictionary.
-     * 
-     * @param dictionary
-     * @param attributeType
+     *
+     * @param dictionary the dictionary
+     * @param attributeType the attribute type
      */
     public void init(String[] dictionary, DataType<?> attributeType) {
         this.dictionary = dictionary;
@@ -258,9 +323,9 @@ public abstract class MicroaggregateFunction implements Serializable {
     
     /**
      * This internal function returns an aggregate value for the given distribution.
-     * 
-     * @param values
-     * @return
+     *
+     * @param values the values
+     * @return the string
      */
     protected abstract String aggregateInternal(Distribution values);
     
