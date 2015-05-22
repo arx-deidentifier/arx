277d4993cbfa40df7e260a3b234200c162ac5565
Florian Kohlmayer <kohlmayer@gmail.com>
2015-05-22 11:04:06 +0200

    Bugfix: fix geometric mean with weights for f***s sake!

--- src/main/org/deidentifier/arx/metric/v2/ILMultiDimensionalGeometricMean.java
+++ src/main/org/deidentifier/arx/metric/v2/ILMultiDimensionalGeometricMean.java
@@ -30,10 +30,10 @@ import org.deidentifier.arx.metric.InformationLoss;
  * @author Florian Kohlmayer
  */
 public class ILMultiDimensionalGeometricMean extends AbstractILMultiDimensionalReduced {
-
+    
     /** SVUID. */
     private static final long serialVersionUID = 621501985571033348L;
-
+    
     /**
      * Creates a new instance.
      *
@@ -44,20 +44,20 @@ public class ILMultiDimensionalGeometricMean extends AbstractILMultiDimensionalR
                                     final double[] weights) {
         super(values, weights);
     }
-
+    
     @Override
     public InformationLoss<double[]> clone() {
         return new ILMultiDimensionalGeometricMean(getValues(),
                                                    getWeights());
     }
-
+    
     @Override
     protected double getAggregate() {
         double[] values = getValues();
         double[] weights = getWeights();
         double result = 1.0d;
         for (int i = 0; i < values.length; i++) {
-            result *= Math.pow((values[i] + 1d) * weights[i], 1.0d / (double) values.length);
+            result *= Math.pow((values[i] * weights[i] + 1d), 1.0d / (double) values.length);
         }
         return result - 1d;
     }
