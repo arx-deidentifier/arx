b6cdaf213f41659d1d77c1f7bde88404c2afdf41
ga82bos <max.zitzmann@tum.de>
2016-09-15 14:47:19 +0200

    example for calculating alpha separation and distinction added

---
+++ src/example/org/deidentifier/arx/examples/Example47.java
@@ -0,0 +1,152 @@
+package org.deidentifier.arx.examples;
+
+import org.apache.commons.lang.StringUtils;
+import org.deidentifier.arx.*;
+import org.deidentifier.arx.risk.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class Example47 extends Example {
+    private static class QuasiIdentifier {
+        private String identifier;
+        private double alphaDistinction;
+        private double alphaSeparation;
+
+        public QuasiIdentifier(String identifier, double alphaDistinction, double alphaSeparation) {
+            this.identifier = identifier;
+            this.alphaDistinction = alphaDistinction;
+            this.alphaSeparation = alphaSeparation;
+        }
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+        public void setIdentifier(String identifier) {
+            this.identifier = identifier;
+        }
+
+        public double getAlphaDistinction() {
+            return alphaDistinction;
+        }
+
+        public void setAlphaDistinction(double alphaDistinction) {
+            this.alphaDistinction = alphaDistinction;
+        }
+
+        public double getAlphaSeparation() {
+            return alphaSeparation;
+        }
+
+        public void setAlphaSeparation(double alphaSeparation) {
+            this.alphaSeparation = alphaSeparation;
+        }
+    }
+
+    /**
+     * Entry point.
+     *
+     * @param args the arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+        Data data = loadData();
+        //Data data = loadCsv();
+
+        // flag every identifier as quasi identifier
+        for (int i = 0; i < data.getHandle().getNumColumns(); i++) {
+            data.getDefinition().setAttributeType(data.getHandle().getAttributeName(i), AttributeType.QUASI_IDENTIFYING_ATTRIBUTE);
+        }
+
+        // Perform risk analysis
+        System.out.println("\n - Input data");
+        print(data.getHandle());
+
+        System.out.println("\n - Quasi-identifiers with values (in percent):");
+        analyzeAttributes(data.getHandle());
+    }
+
+    /**
+     * Calculate Alpha Distinction and Separation
+     *
+     * @param handle the data handle
+     */
+    private static void analyzeAttributes(DataHandle handle) {
+        ARXPopulationModel populationmodel = ARXPopulationModel.create(ARXPopulationModel.Region.USA);
+        RiskEstimateBuilder builder = handle.getRiskEstimator(populationmodel);
+        RiskModelAttributes riskmodel = builder.getAttributeRisks();
+
+        ArrayList<QuasiIdentifier> quasiIdentifiers = new ArrayList<>();
+        for (RiskModelAttributes.QuasiIdentifierRisk risk : riskmodel.getAttributeRisks()) {
+            quasiIdentifiers.add(new QuasiIdentifier(risk.getIdentifier().toString(), risk.getDistinction(), risk.getSeparation()));
+        }
+        // output
+        printPrettyTable(quasiIdentifiers);
+    }
+
+    private static Data loadCsv() throws IOException {
+        String csvFile = "data/example.csv";
+
+        DataSource source = DataSource.createCSVSource(csvFile, StandardCharsets.UTF_8, ';', true);
+        source.addColumn("sex", DataType.STRING);
+        source.addColumn("age", DataType.INTEGER);
+        source.addColumn("race", DataType.STRING);
+        source.addColumn("marital-status", DataType.STRING);
+        source.addColumn("education", DataType.STRING);
+        source.addColumn("native-country", DataType.STRING);
+        source.addColumn("workclass", DataType.STRING);
+        source.addColumn("occupation", DataType.STRING);
+        source.addColumn("salary-class", DataType.STRING);
+        source.addColumn("ldl", DataType.STRING);
+        return Data.create(source);
+    }
+
+    private static Data loadData() {
+        // Define data
+        Data.DefaultData data = Data.create();
+        data.add("age", "sex", "state");
+        data.add("20", "Female", "CA");
+        data.add("30", "Female", "CA");
+        data.add("40", "Female", "TX");
+        data.add("20", "Male", "NY");
+        data.add("40", "Male", "CA");
+
+        data.add("53", "Male", "CA");
+        data.add("76", "Male", "EU");
+        data.add("40", "Female", "AS");
+        data.add("32", "Female", "CA");
+        data.add("88", "Male", "CA");
+        data.add("48", "Female", "AS");
+        data.add("76", "Male", "UU");
+        return data;
+    }
+
+    private static void printPrettyTable(ArrayList<QuasiIdentifier> quasiIdentifiers) {
+        // get char count of longest quasi-identifier
+        int charCountLongestQi = quasiIdentifiers.get(quasiIdentifiers.size() - 1).getIdentifier().length();
+
+        // make sure that there is enough space for the table header strings
+        charCountLongestQi = Math.max(charCountLongestQi, 12);
+
+        // calculate space needed
+        String leftAlignFormat = "| %-" + charCountLongestQi + "s | %13.2f | %12.2f |%n";
+
+        // add 2 spaces that are in the string above on the left and right side of the first pattern
+        charCountLongestQi += 2;
+
+        // subtract the char count of the column header string to calculate
+        // how many spaces we need for filling up to the right columnborder
+        int spacesAfterColumHeader = charCountLongestQi - 12;
+
+        System.out.format("+" + StringUtils.repeat("-", charCountLongestQi) + "+---------------+--------------+%n");
+        System.out.format("| Identifier " + StringUtils.repeat(" ", spacesAfterColumHeader) + "| α-Distinction | α-Separation |%n");
+        System.out.format("+" + StringUtils.repeat("-", charCountLongestQi) + "+---------------+--------------+%n");
+        for (QuasiIdentifier quasiIdentifier : quasiIdentifiers) {
+            // print every Quasi-Identifier
+            System.out.format(leftAlignFormat, quasiIdentifier.getIdentifier(), quasiIdentifier.alphaDistinction * 100, quasiIdentifier.alphaSeparation * 100);
+        }
+        System.out.format("+" + StringUtils.repeat("-", charCountLongestQi) + "+---------------+--------------+%n");
+    }
+}
