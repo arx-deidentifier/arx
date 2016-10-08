cae95ae18e2652c269821857eebce327d2e0d2ff
Fabian Prasser <mail@fabian-prasser.de>
2016-10-08 23:18:02 +0200

    Add some further statistics

--- src/main/org/deidentifier/arx/risk/msu/SUDA2Result.java
+++ src/main/org/deidentifier/arx/risk/msu/SUDA2Result.java
@@ -17,6 +17,7 @@
 package org.deidentifier.arx.risk.msu;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.Set;
 
 /**
@@ -33,7 +34,13 @@ public class SUDA2Result {
     /** The number of MSUs */
     private int         numMSUs = 0;
     /** Contributions of each column */
-    private final int[] columnContributions;
+    private final int[] columnKeyContributions;
+    /** Contributions of each column */
+    private final int[] columnKeyMinSize;
+    /** Contributions of each column */
+    private final int[] columnKeyMaxSize;
+    /** Contributions of each column */
+    private final int[] columnKeyAverageSize;
     /** Distribution of sizes of MSUs */
     private final int[] sizeDistribution;
     
@@ -44,7 +51,11 @@ public class SUDA2Result {
      */
     SUDA2Result(int columns, int maxK) {
         this.columns = columns;
-        this.columnContributions = new int[columns];
+        this.columnKeyContributions = new int[columns];
+        this.columnKeyMinSize = new int[columns];
+        Arrays.fill(columnKeyMinSize, columns);
+        this.columnKeyMaxSize = new int[columns];
+        this.columnKeyAverageSize = new int[columns];
         this.maxK = maxK;
         this.sizeDistribution = new int[maxK];
     }
@@ -53,8 +64,8 @@ public class SUDA2Result {
      * Returns the contributions of each column
      * @return
      */
-    public int[] getColumnContributions() {
-        return this.columnContributions;
+    public int[] getColumnKeyContributions() {
+        return this.columnKeyContributions;
     }
 
     /**
@@ -85,20 +96,38 @@ public class SUDA2Result {
      * Returns the distribution of the sizes of MSUs
      * @return
      */
-    public int[] getSizeDistribution() {
+    public int[] getKeySizeDistribution() {
         return this.sizeDistribution;
     }
     
     @Override
     public String toString() {
+        
+        int[] totals1 = new int[columns];
+        Arrays.fill(totals1, numMSUs);
+        
+        int[] totals2 = new int[columns];
+        for (int i = 0; i < totals2.length; i++) {
+            totals2[i] = columnKeyContributions[i];
+        }
+        
+        int[] totals3 = new int[maxK];
+        Arrays.fill(totals3, numMSUs);
+        
         StringBuilder builder = new StringBuilder();
         builder.append("SUDA2 result\n");
         builder.append(" - Number of columns: ").append(this.columns).append("\n");
         builder.append(" - Number of MSUs: ").append(this.numMSUs).append("\n");
-        builder.append(" - Column contributions\n");
-        builder.append(toString("     ", columnContributions, 0));
+        builder.append(" - Column key contributions\n");
+        builder.append(toString("     ", columnKeyContributions, totals1, 0, false, true));
+        builder.append(" - Column key min. size\n");
+        builder.append(toString("     ", columnKeyMinSize, null, 0, true, false));
+        builder.append(" - Column key max. size\n");
+        builder.append(toString("     ", columnKeyMaxSize, null, 0, true, false));
+        builder.append(" - Column key avg. size\n");
+        builder.append(toString("     ", columnKeyAverageSize, totals2, 0, false, true));
         builder.append(" - Size distribution\n");
-        builder.append(toString("     ", sizeDistribution, 1));
+        builder.append(toString("     ", sizeDistribution, totals3, 1, true, true));
         return builder.toString();
     }
     
@@ -120,9 +149,10 @@ public class SUDA2Result {
      * @param intent
      * @param array
      * @param offset
+     * @param absolute
      * @return
      */
-    private String toString(String intent, int[] array, int offset) {
+    private String toString(String intent, int[] array, int[] totals, int offset, boolean absolute, boolean relative) {
         
         StringBuilder builder = new StringBuilder();
         DecimalFormat integerFormat = new DecimalFormat("#######");
@@ -132,27 +162,37 @@ public class SUDA2Result {
         for (int index = 0; index < array.length; index++) {
             builder.append(toString(integerFormat.format(index + offset), VALUE_WIDTH)).append("|");
         }
-        builder.append("\n").append(intent);
-        int width = builder.length() - 2 * intent.length() - 1;
-        for (int i = 0; i < width; i++) {
-            builder.append("-");
-        }
-        builder.append("\n").append(intent).append("|");
-        for (int val : array) {
-            String value = integerFormat.format(val);
-            builder.append(toString(value, VALUE_WIDTH)).append("|");
-        }
-        builder.append("\n").append(intent);
-        for (int i = 0; i < width; i++) {
-            builder.append("-");
-        }
-        builder.append("\n").append(intent).append("|");
-        for (double val : array) {
-            String value = doubleFormat.format(val / numMSUs).replace(',', '.');
-            if (value.equals("0") && val > 0) value = "~0";
-            builder.append(toString(value, VALUE_WIDTH)).append("|");
-        }
+        int width = builder.length() - intent.length();
         builder.append("\n");
+        if (absolute) {
+            builder.append(intent);
+            for (int i = 0; i < width; i++) {
+                builder.append("-");
+            }
+            builder.append("\n");
+            builder.append(intent).append("|");
+            for (int val : array) {
+                String value = integerFormat.format(val);
+                builder.append(toString(value, VALUE_WIDTH)).append("|");
+            }
+            builder.append("\n");
+        }
+        if (relative) {
+            builder.append(intent);
+            for (int i = 0; i < width; i++) {
+                builder.append("-");
+            }
+            builder.append("\n");
+            builder.append(intent).append("|");
+            for (int i = 0; i < array.length; i++) {
+                int _value = array[i];
+                int _total = totals[i];
+                String value = doubleFormat.format((double)_value / (double)_total).replace(',', '.');
+                if (value.equals("0") && _value > 0) value = "~0";
+                builder.append(toString(value, VALUE_WIDTH)).append("|");
+            }
+            builder.append("\n");
+        }
         return builder.toString();
     }
 
@@ -164,7 +204,8 @@ public class SUDA2Result {
         this.numMSUs++;
         this.sizeDistribution[set.size() - 1]++;
         for (SUDA2Item item : set) {
-            this.columnContributions[item.getColumn()]++;
+            int column = item.getColumn();
+            registerColumn(column, set.size());
         }
     }
 
@@ -178,9 +219,10 @@ public class SUDA2Result {
         this.sizeDistribution[set.size()]++;
         int size = set.size();
         for (int i = 0; i < size; i++) {
-            this.columnContributions[set.get(i).getColumn()]++;
+            int column = set.get(i).getColumn();
+            registerColumn(column, set.size() + 1);
         }
-        this.columnContributions[item.getColumn()]++;
+        registerColumn(item.getColumn(), set.size() + 1);
     }
 
     /**
@@ -192,7 +234,20 @@ public class SUDA2Result {
         this.sizeDistribution[set.size()-1]++;
         int size = set.size();
         for (int i = 0; i < size; i++) {
-            this.columnContributions[set.get(i).getColumn()]++;
+            int column = set.get(i).getColumn();
+            registerColumn(column, set.size());
         }
+    }
+
+    /**
+     * Registers an MSU with a column
+     * @param column
+     * @param size
+     */
+    private void registerColumn(int column, int size) {
+        this.columnKeyContributions[column]++;
+        this.columnKeyMinSize[column] = Math.min(this.columnKeyMinSize[column], size);
+        this.columnKeyMaxSize[column] = Math.max(this.columnKeyMaxSize[column], size);
+        this.columnKeyAverageSize[column] += size;
     }
 }
