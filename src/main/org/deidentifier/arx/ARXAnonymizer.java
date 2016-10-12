29525ed7d529324a99ed72411a7dffaad1c34724
Fabian Prasser <mail@fabian-prasser.de>
2016-10-13 00:21:20 +0200

    Fix parameterization checks

--- src/main/org/deidentifier/arx/ARXAnonymizer.java
+++ src/main/org/deidentifier/arx/ARXAnonymizer.java
@@ -367,12 +367,14 @@ public class ARXAnonymizer {
 
         if (config.containsCriterion(KAnonymity.class)){
             KAnonymity c = config.getCriterion(KAnonymity.class);
+            // TODO: getDataGeneralized().getDataLength() does not consider data subsets
             if ((c.getK() > manager.getDataGeneralized().getDataLength()) || (c.getK() < 1)) { 
                 throw new IllegalArgumentException("Parameter k (" + c.getK() + ") musst be >=1 and less or equal than the number of rows (" + manager.getDataGeneralized().getDataLength()+")"); 
             }
         }
         if (config.containsCriterion(LDiversity.class)){
             for (LDiversity c : config.getCriteria(LDiversity.class)){
+                // TODO: getDataGeneralized().getDataLength() does not consider data subsets
 	            if ((c.getL() > manager.getDataGeneralized().getDataLength()) || (c.getL() < 1)) { 
 	                throw new IllegalArgumentException("Parameter l (" + c.getL() + ") musst be >=1 and less or equal than the number of rows (" + manager.getDataGeneralized().getDataLength()+")"); 
 	            }
@@ -380,7 +382,7 @@ public class ARXAnonymizer {
         }
         if (config.containsCriterion(DDisclosurePrivacy.class)){
             for (DDisclosurePrivacy c : config.getCriteria(DDisclosurePrivacy.class)){
-                if (c.getD() < 0) { 
+                if (c.getD() <= 0) { 
                     throw new IllegalArgumentException("Parameter d (" + c.getD() + ") musst be positive and larger than 0"); 
                 }
             }
