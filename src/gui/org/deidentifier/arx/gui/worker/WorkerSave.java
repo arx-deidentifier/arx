da3dadcc81bebf772f8ab21733db4eaeb9909afa
eicherj <johanna.eicher@gmail.com>
2017-04-04 09:47:45 +0200

    Bug: NPE when saving project without data.
    Fix: Add null-checks when writing/loading project.

--- src/gui/org/deidentifier/arx/gui/worker/WorkerSave.java
+++ src/gui/org/deidentifier/arx/gui/worker/WorkerSave.java
@@ -504,20 +504,22 @@ public class WorkerSave extends Worker<Model> {
         else definition = model.getOutputDefinition();
         
         // Store all from definition that have not yet been stored
-        DataHandle handle = config.getInput().getHandle();
-        for (int i = 0; i < handle.getNumColumns(); i++) {
-            final String attr = handle.getAttributeName(i);
-            
-            // Do we have a hierarchy
-            if (!saved.contains(attr) && definition.getHierarchy(attr) != null && 
-                definition.getHierarchy(attr).length != 0 &&
-                definition.getHierarchy(attr)[0].length != 0) {
+        if(config.getInput() != null) {
+            DataHandle handle = config.getInput().getHandle();
+            for (int i = 0; i < handle.getNumColumns(); i++) {
+                final String attr = handle.getAttributeName(i);
                 
-                // Store this hierarchy
-                zip.putNextEntry(new ZipEntry(prefix + "hierarchies/" + toFileName(attr) + ".csv")); //$NON-NLS-1$ //$NON-NLS-2$
-                CSVDataOutput out = new CSVDataOutput(zip, model.getCSVSyntax().getDelimiter());
-                out.write(definition.getHierarchy(attr));
-                saved.add(attr);
+                // Do we have a hierarchy
+                if (!saved.contains(attr) && definition.getHierarchy(attr) != null && 
+                    definition.getHierarchy(attr).length != 0 &&
+                    definition.getHierarchy(attr)[0].length != 0) {
+                    
+                    // Store this hierarchy
+                    zip.putNextEntry(new ZipEntry(prefix + "hierarchies/" + toFileName(attr) + ".csv")); //$NON-NLS-1$ //$NON-NLS-2$
+                    CSVDataOutput out = new CSVDataOutput(zip, model.getCSVSyntax().getDelimiter());
+                    out.write(definition.getHierarchy(attr));
+                    saved.add(attr);
+                }
             }
         }
     }
