f0f4609200bb54aea89c04fa44fb52f3f81cb6f5
eicherj <johanna.eicher@gmail.com>
2017-04-04 10:56:27 +0200

    Format.

--- src/gui/org/deidentifier/arx/gui/worker/WorkerSave.java
+++ src/gui/org/deidentifier/arx/gui/worker/WorkerSave.java
@@ -510,12 +510,12 @@ public class WorkerSave extends Worker<Model> {
                 final String attr = handle.getAttributeName(i);
 
                 // Do we have a hierarchy
-                if (!saved.contains(attr) && definition.getHierarchy(attr) != null &&
+                if (!saved.contains(attr) && definition.getHierarchy(attr) != null && 
                     definition.getHierarchy(attr).length != 0 &&
                     definition.getHierarchy(attr)[0].length != 0) {
 
                     // Store this hierarchy
-                    zip.putNextEntry(new ZipEntry(prefix + "hierarchies/" + toFileName(attr) + ".csv")); //$NON-NLS-1$ //$NON-NLS-1$
+                    zip.putNextEntry(new ZipEntry(prefix + "hierarchies/" + toFileName(attr) + ".csv")); //$NON-NLS-1$ //$NON-NLS-2$
                     CSVDataOutput out = new CSVDataOutput(zip, model.getCSVSyntax().getDelimiter());
                     out.write(definition.getHierarchy(attr));
                     saved.add(attr);
