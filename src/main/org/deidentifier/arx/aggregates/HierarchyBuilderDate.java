125153542ca108eb6c546758c407c215cde7dfad
Fabian Prasser <mail@fabian-prasser.de>
2017-05-29 12:06:34 +0200

    Code formatting

--- src/main/org/deidentifier/arx/aggregates/HierarchyBuilderDate.java
+++ src/main/org/deidentifier/arx/aggregates/HierarchyBuilderDate.java
@@ -25,7 +25,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.TimeZone;
