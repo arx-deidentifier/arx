c8f4cde8c1cb4a070c2290f41d2d36224434288d
eicherj <johanna.eicher@gmail.com>
2017-10-05 14:44:13 +0200

    Format code.

--- src/test/org/deidentifier/arx/test/TestClassification.java
+++ src/test/org/deidentifier/arx/test/TestClassification.java
@@ -1,3 +1,20 @@
+/*
+ * ARX: Powerful Data Anonymization
+ * Copyright 2012 - 2017 Fabian Prasser, Florian Kohlmayer and contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.deidentifier.arx.test;
 
 import static org.junit.Assert.assertEquals;
@@ -50,7 +67,7 @@ public class TestClassification {
      * @return
      * @throws IOException
      */
-    private Data getData(String dataset) throws IOException {
+    private Data getData(final String dataset) throws IOException {
 
         // Load data
         Data data = Data.create("data/" + dataset + ".csv", StandardCharsets.UTF_8, ';');
@@ -106,12 +123,12 @@ public class TestClassification {
      * @throws IOException
      */
     private ARXResult getResult() throws IOException {
+        
         if (result == null) {
 
             // Data
             Data data = getData("adult");
-            data.getDefinition().setAttributeType("marital-status",
-                                                  AttributeType.INSENSITIVE_ATTRIBUTE);
+            data.getDefinition().setAttributeType("marital-status", AttributeType.INSENSITIVE_ATTRIBUTE);
             data.getDefinition().setDataType("age", DataType.INTEGER);
 
             // Config
