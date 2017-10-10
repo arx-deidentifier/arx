a3629de6396541beb502e46aaee2ac4356e2a371
Fabian Prasser <mail@fabian-prasser.de>
2017-10-10 19:58:41 +0200

    Add exception to facilitate later implementation

--- src/main/org/deidentifier/arx/framework/data/DataManager.java
+++ src/main/org/deidentifier/arx/framework/data/DataManager.java
@@ -127,6 +127,7 @@ public class DataManager {
         if (qualityModel.isAggregatedInputRequired()) { // Only analyze aggregated variables, if required by the quality model
             attributesAggregated.addAll(definition.getQuasiIdentifiersWithMicroaggregation());
             attributesAggregated.addAll(definition.getQuasiIdentifiersWithGeneralization());
+            throw new RuntimeException("Not implemented"); // TODO
         }
         attributesResponse.removeAll(attributesGeneralized); // Do not analyze generalized response variables
         attributesAnalyzed.addAll(attributesAggregated);
