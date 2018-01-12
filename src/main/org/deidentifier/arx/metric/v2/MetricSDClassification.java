f17822f6c5834aaeb98d194d2381aa0049e532d5
Fabian Prasser <mail@fabian-prasser.de>
2018-01-12 18:16:32 +0100

    Bugfix: correctly handle non-qi target variables

--- src/main/org/deidentifier/arx/metric/v2/MetricSDClassification.java
+++ src/main/org/deidentifier/arx/metric/v2/MetricSDClassification.java
@@ -42,19 +42,21 @@ import org.deidentifier.arx.metric.MetricConfiguration;
 public class MetricSDClassification extends AbstractMetricSingleDimensional {
 
     /** SVUID. */
-    private static final long serialVersionUID             = -7940144844158472876L;
+    private static final long serialVersionUID          = -7940144844158472876L;
 
     /** Indices of response variables in distributions */
-    private int[]             responseVariables            = null;
-    /** Number of response variables in quasi-identifiers */
-    private int               responseVariablesNotAnalyzed = 0;
+    private int[]             responseVariablesNonQI    = null;
+    /** Indices of response variables in quasi-identifiers */
+    private int[]             responseVariablesQI       = null;
 
     /** Penalty */
-    private double            penaltySuppressed            = 1d;
+    private double            penaltySuppressed         = 1d;
     /** Penalty */
-    private double            penaltyInfrequentResponse    = 1d;
+    private double            penaltyInfrequentResponse = 1d;
     /** Penalty */
-    private double            penaltyNoMajorityResponse    = 1d;
+    private double            penaltyNoMajorityResponse = 1d;
+    /** Maximal penality*/
+    private double            penaltyMax;
 
     /**
      * Creates a new instance.
@@ -74,24 +76,7 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
 
     @Override
     public ILSingleDimensional createMaxInformationLoss() {
-        Double rows = getNumTuples();
-        if (rows == null) {
-            
-            throw new IllegalStateException("Metric must be initialized first");
-            
-        } else {
-            
-            // TODO: This is probably crap. Non-analyzed RVs need to be treated differently.
-            // Non-analyzed response variables are only penalized if they are suppressed
-            double max = rows * responseVariablesNotAnalyzed * penaltySuppressed;
-            
-            // Use maximal penalty for other response variables
-            double maxPenalty = Math.max(penaltySuppressed, Math.max(penaltyInfrequentResponse, penaltyNoMajorityResponse));
-            max += rows * responseVariables.length * maxPenalty;
-            
-            // Done
-            return new ILSingleDimensional(max);
-        }
+        return new ILSingleDimensional(1d);
     }
     
     @Override
@@ -158,12 +143,12 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
     }
     
     /**
-     * Returns the penalty for the given entry
+     * Returns the sum of penalties for all non-QI target variables
      * @param entry
      * @return
      */
-    private double getPenalty(HashGroupifyEntry entry) {
-
+    private double getPenaltyNonQI(HashGroupifyEntry entry) {
+        
         // Prepare
         double result = 0d;
         
@@ -171,13 +156,13 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
         if (!entry.isNotOutlier) {
             
             // According penalty for all records and response variables in this class
-            result += entry.count * (responseVariablesNotAnalyzed + responseVariables.length) * penaltySuppressed;
+            result += entry.count * responseVariablesNonQI.length * (penaltySuppressed / penaltyMax);
             
         // Not suppressed
         } else {
             
             // For each analyzed response variable
-            for (int index : this.responseVariables) {
+            for (int index : this.responseVariablesNonQI) {
                 
                 // Find frequencies of most frequent and second most frequent attribute values
                 int top1 = -1;
@@ -188,38 +173,39 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
                 int[] buckets = distribution.getBuckets();
                 for (int i = 0; i < buckets.length; i += 2) {
                     
-                    // Get frequency
-                    int frequency = buckets[i + 1];
-                    boolean largerThanTop1 = frequency > top1;
-                    boolean largerThanTop2 = frequency > top2;
+                    // bucket not empty
+                    if (buckets[i] != -1) { 
                     
-                    // Step 1: If frequency is > top1 
-                    //         --> top1 is moved down to top2
-                    top2 = largerThanTop1 ? top1 : top2;
-    
-                    // Step 2: If frequency is > top1 
-                    //         --> top1 is set to new frequency
-                    top1 = largerThanTop1 ? frequency : top1;
-                    
-                    // Step 3: If frequency is > top2 but not > top1 (which implies frequency != top1, because of step 2)
-                    //         --> top2 is set to new frequency
-                    top2 = largerThanTop2 && frequency != top1 ? frequency : top2;
+                        // Get frequency
+                        int frequency = buckets[i + 1];
+                        boolean largerThanTop1 = frequency > top1;
+                        boolean largerThanTop2 = frequency > top2;
+                        
+                        // Step 1: If frequency is > top1 
+                        //         --> top1 is moved down to top2
+                        top2 = largerThanTop1 ? top1 : top2;
+        
+                        // Step 2: If frequency is > top1 
+                        //         --> top1 is set to new frequency
+                        top1 = largerThanTop1 ? frequency : top1;
+                        
+                        // Step 3: If frequency is > top2 but not > top1
+                        //         --> top2 is set to new frequency
+                        top2 = largerThanTop2 && !largerThanTop1 ? frequency : top2;
+                    }
                 }
                 
                 // If a majority class label exists
                 if (top1 != top2) {
-                    
                     // Records with other than majority class label get penalized
-                    result += (entry.count - top1) * penaltyInfrequentResponse;
+                    result += (entry.count - top1) * (penaltyInfrequentResponse / penaltyMax);
                 } else {
-                    
                     // All records get penalized
-                    result += entry.count * penaltyNoMajorityResponse;
+                    result += entry.count * (penaltyNoMajorityResponse / penaltyMax);
                 }
-                
-                // TODO: Non-analyzed RVs need to also be treated.
             }
         }
+        
         
         // Return overall penalty
         return result;
@@ -227,7 +213,7 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
     
     @Override
     protected ILSingleDimensionalWithBound getInformationLossInternal(final Transformation node, final HashGroupify g) {
-       
+
         // Prepare
         double penalty = 0d;
         HashGroupifyEntry m = g.getFirstEquivalenceClass();
@@ -235,7 +221,7 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
         // For each group
         while (m != null) {
             if (m.count > 0) {
-                penalty += getPenalty(m);
+                penalty += getPenaltyNonQI(m);
             }
             m = m.nextOrdered;
         }
@@ -249,8 +235,9 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
     protected ILSingleDimensionalWithBound getInformationLossInternal(Transformation node, HashGroupifyEntry m) {
 
         if (m.count > 0) {
+            
             // TODO: Can a lower bound be calculated for this model?
-            return createInformationLoss(getPenalty(m));
+            return createInformationLoss(getPenaltyNonQI(m));
         } else {
             return createInformationLoss(0d);
         }
@@ -278,10 +265,34 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
         // Super
         super.initializeInternal(manager, definition, input, hierarchies, config);
         
+        // Extract response variables
+        this.responseVariablesNonQI = getIndices(definition, manager.getDataAnalyzed());
+        this.responseVariablesQI = getIndices(definition, manager.getDataGeneralized());
+
+        // TODO: Remove when implemented
+        if (this.responseVariablesQI.length != 0) {
+            throw new UnsupportedOperationException();
+        }
+        
+        // Set penalties using the gs-factor. This is sort of a hack but should be OK for now.
+        penaltySuppressed            = super.getGeneralizationSuppressionFactor();
+        penaltyInfrequentResponse    = super.getGeneralizationFactor();
+        penaltyNoMajorityResponse    = super.getSuppressionFactor();
+        penaltyMax = Math.max(penaltySuppressed, Math.max(penaltyInfrequentResponse, penaltyNoMajorityResponse)) * super.getNumTuples();
+    }
+
+    /**
+     * Returns sorted indices of response variables in the given data
+     * @param definition
+     * @param data
+     * @return
+     */
+    private int[] getIndices(DataDefinition definition, Data data) {
+
         // Extract indices of response variables
         List<Integer> indices = new ArrayList<>();
         for (String variable : definition.getResponseVariables()){
-            int index = manager.getDataAnalyzed().getIndexOf(variable);
+            int index = data.getIndexOf(variable);
             if (index != -1) {
                 indices.add(index);
             }
@@ -289,17 +300,12 @@ public class MetricSDClassification extends AbstractMetricSingleDimensional {
         
         // Store information about response variables
         Collections.sort(indices);
-        this.responseVariables = new int[indices.size()];
+        int[] result = new int[indices.size()];
         for (int i = 0; i < indices.size(); i++) {
-            responseVariables[i] = indices.get(i);
+            result[i] = indices.get(i);
         }
         
-        // TODO: This is probably crap. Non-analyzed RVs need to be treated differently.
-        this.responseVariablesNotAnalyzed = definition.getResponseVariables().size() - responseVariables.length;
-        
-        // Set penalties using the gs-factor. This is sort of a hack but should be OK for now.
-        penaltySuppressed            = super.getSuppressionFactor();
-        penaltyInfrequentResponse    = super.getGeneralizationFactor();
-        penaltyNoMajorityResponse    = super.getGeneralizationSuppressionFactor();
+        // Return
+        return result;
     }
 }
