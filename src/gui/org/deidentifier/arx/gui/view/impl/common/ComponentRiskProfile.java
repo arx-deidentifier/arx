591aa33ad8b31eba28e3c8a4cbce3443a1551ee0
Fabian Prasser <mail@fabian-prasser.de>
2019-01-16 16:42:31 +0100

    Minor bugfixes to prevent problems due to rounding issues

--- src/gui/org/deidentifier/arx/gui/view/impl/common/ComponentRiskProfile.java
+++ src/gui/org/deidentifier/arx/gui/view/impl/common/ComponentRiskProfile.java
@@ -111,17 +111,19 @@ public class ComponentRiskProfile {
             return result;
         }
     }
+    
     /**
      * Checks a given series
      * @param values
      */
     private static void checkSeries(double[] values) {
         for (double value : values) {
-            if (value <0d || value>1d) {
-                throw new IllegalArgumentException("Values must be in range [0, 1]");
+            if (value < 0d) {
+                throw new IllegalArgumentException("Values must be >= 0 but is " + value);
             }
         }
     }
+    
     /**
      * Insert item to back
      * @param array
