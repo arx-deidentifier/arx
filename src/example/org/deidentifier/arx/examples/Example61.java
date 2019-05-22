fa9daf6f9fc2d9beb14e328aac1f226b1dd07764
Fabian Prasser <mail@fabian-prasser.de>
2019-05-22 16:40:30 +0200

    Add missing example

---
+++ src/example/org/deidentifier/arx/examples/Example61.java
@@ -0,0 +1,96 @@
+/*
+ * ARX: Powerful Data Anonymization
+ * Copyright 2012 - 2018 Fabian Prasser and contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.deidentifier.arx.examples;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.deidentifier.arx.ARXPopulationModel;
+import org.deidentifier.arx.ARXPopulationModel.Region;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+import org.deidentifier.arx.Data;
+import org.deidentifier.arx.io.CSVHierarchyInput;
+import org.deidentifier.arx.risk.RiskModelMSUKeyStatistics;
+
+/**
+ * This class implements an example on how to evaluate risks with SUDA2
+ *
+ * @author Fabian Prasser
+ * @author Florian Kohlmayer
+ */
+public class Example61 extends Example {
+    
+    /**
+     * Loads a data set from disk
+     * @param dataset
+     * @return
+     * @throws IOException
+     */
+    public static Data createData(final String dataset) throws IOException {
+        
+        // Load data
+        Data data = Data.create("data/" + dataset + ".csv", StandardCharsets.UTF_8, ';');
+        
+        // Read generalization hierarchies
+        FilenameFilter hierarchyFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                if (name.matches(dataset + "_hierarchy_(.)+.csv")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        
+        // Create definition
+        File testDir = new File("data/");
+        File[] genHierFiles = testDir.listFiles(hierarchyFilter);
+        Pattern pattern = Pattern.compile("_hierarchy_(.*?).csv");
+        for (File file : genHierFiles) {
+            Matcher matcher = pattern.matcher(file.getName());
+            if (matcher.find()) {
+                CSVHierarchyInput hier = new CSVHierarchyInput(file, StandardCharsets.UTF_8, ';');
+                String attributeName = matcher.group(1);
+                data.getDefinition().setAttributeType(attributeName, Hierarchy.create(hier.getHierarchy()));
+            }
+        }
+        
+        return data;
+    }
+    
+    /**
+     * Entry point.
+     * 
+     * @param args the arguments
+     * @throws ParseException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws ParseException, IOException {
+    
+        Data data = createData("adult");
+        long time = System.currentTimeMillis();
+        RiskModelMSUKeyStatistics risks = data.getHandle().getRiskEstimator(ARXPopulationModel.create(Region.USA)).getMSUKeyStatistics();
+        System.out.println("Time: " + (System.currentTimeMillis() - time));
+        System.out.println(risks.getNumKeys());
+    }
+}
