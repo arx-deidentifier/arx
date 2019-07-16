b1ba30d644783009ff399e5cc6e222c1c7e54316
Fabian Prasser <mail@fabian-prasser.de>
2019-07-16 15:28:39 +0200

    Bugfix

--- src/gui/org/deidentifier/arx/gui/Controller.java
+++ src/gui/org/deidentifier/arx/gui/Controller.java
@@ -2034,7 +2034,9 @@ public class Controller implements IView {
 
     @Override
     public void reset() {
-        model.reset();
+        if (model != null) {
+            model.reset();
+        }
         for (final Set<IView> listeners : getListeners().values()) {
             for (final IView listener : listeners) {
                 listener.reset();
