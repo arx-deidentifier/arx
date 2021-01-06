654db08bcbf2a8a3f0c3266bfad13058bdf7a874
Fabian Prasser <mail@fabian-prasser.de>
2021-01-06 15:48:04 +0100

    Try update service for MacOS

--- src/gui/org/deidentifier/arx/gui/view/impl/common/ComponentTabFolder.java
+++ src/gui/org/deidentifier/arx/gui/view/impl/common/ComponentTabFolder.java
@@ -19,9 +19,15 @@ package org.deidentifier.arx.gui.view.impl.common;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import org.deidentifier.arx.gui.view.SWTUtil;
 import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
 
 /**
  * This wrapper around CTabFolder fixes SWT bug 507611 for ARX
@@ -38,6 +44,101 @@ public class ComponentTabFolder extends CTabFolder {
      */
     public ComponentTabFolder(Composite parent, int style) {
         super(parent, style);
+        
+        // Add refresh functionality for MacOS
+        if (SWTUtil.isMac() && parent instanceof Shell) {
+            
+            // Print
+            System.out.println("Installing update service");
+            
+            // Register trigger
+            this.addSelectionListener(new SelectionListener() {
+
+                @Override
+                public void widgetDefaultSelected(SelectionEvent arg0) {
+                    widgetSelected(arg0);
+                }
+
+                @Override
+                public void widgetSelected(SelectionEvent arg0) {
+                    
+                    CTabItem item = ComponentTabFolder.this.getSelection();
+                    if (item == null) {
+                        return;
+                    }
+                    
+                    ComponentTabFolder.this.getDisplay().timerExec(1000, new Runnable() {
+                       @Override
+                        public void run() {
+                           System.out.println("Forcing redraw on folder: " + item.getText());
+                            try {
+                                forceRedraw(ComponentTabFolder.this);
+                            } catch (NoSuchMethodException | SecurityException
+                                    | IllegalAccessException | IllegalArgumentException
+                                    | InvocationTargetException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    /**
+     * Force redraw on anything
+     * @param componentTabFolder
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws IllegalAccessException 
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     */
+    private void forceRedraw(Control control) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        if (control instanceof CTabFolder) {
+            redraw((CTabFolder) control);
+        } else {
+            redraw(control);
+        }
+        if (control instanceof Composite) {
+            for (Control c : ((Composite) control).getChildren()) {
+                forceRedraw(c);
+            }
+        }
+    } 
+
+    /**
+     * Redraw
+     * @param control
+     */
+    private void redraw(Control control) {
+        control.update();
+        control.redraw();
+    }
+
+    /**
+     * Redraw a tab folder
+     * @param folder
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws IllegalAccessException 
+     */
+    private void redraw(CTabFolder folder) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+        // Request layout
+        folder.requestLayout();
+
+        // Update
+        Method updateFolder = this.getClass().getSuperclass().getDeclaredMethod("updateFolder", int.class);
+        updateFolder.setAccessible(true);
+        updateFolder.invoke(folder, (1 << 1) | (1 << 2) | (1 << 3));
+        
+        // Redraw
+        folder.update();
+        folder.layout(true, true);
+        folder.redraw();
     }
 
     @Override
