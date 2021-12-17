d64045f5beafdbbe0803d3ca3684bbaa83a1499c
idhamari <idhamari@uni-koblenz.de>
2021-12-17 15:30:20 +0100

    fix older format!

--- src/main/org/deidentifier/arx/aggregates/HierarchyBuilderDate.java
+++ src/main/org/deidentifier/arx/aggregates/HierarchyBuilderDate.java
@@ -539,10 +539,10 @@ public class HierarchyBuilderDate extends HierarchyBuilder<Date> implements Seri
             int dateUnit = Integer.valueOf(sdf.format(date));
             int lower    = Integer.valueOf((dateUnit - 1) / (_range))  * (_range) + 1 ;
             int upper    = lower + _range;
-            String outputDate = "[" + lower + "-" + upper + "[";            
+            String outputDate = "[" + lower + ", " + upper + "[";           
             // handle QUARTER_YEAR option
             if (granularity.toString() == "QUARTER_YEAR"){
-                outputDate = input.substring(0,4) + " [" + lower + "-" + upper + "[";
+                outputDate = input.substring(0,4) + " [" + lower + ", " + upper + "[";
             }
             return outputDate;
         }
